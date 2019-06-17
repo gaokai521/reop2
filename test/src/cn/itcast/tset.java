5daec410c68061e52368b8e8b7e62c10f35e72d4
tomcat <tomcat@qq.com>
2019-06-17 10:28:56 +0800

    添加Demo项目

---
+++ test/src/cn/itcast/tset.java
@@ -0,0 +1,15 @@
+package cn.itcast;
+
+public class tset {
+    public static void main(String[] args) {
+        System.out.println(8888888);
+        System.out.println(8888888);
+        System.out.println(8888888);
+        System.out.println(8888888);
+        System.out.println(8888888);
+        System.out.println(8888888);
+        System.out.println(8888888);
+        System.out.println(8888888);
+        System.out.println(8888888);
+    }
+}
